954388483cc81abd4d34c0302f56727c433364be
Carlos-M301 <a173809@une.edu.mx>
2020-09-10 18:17:29 -0500

    Cambios en main y carpeta de evidencia añadida

--- lib/main.dart
+++ lib/main.dart
@@ -9,109 +9,190 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tres en línea',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: juegoGato(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class juegoGato extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState(){
+    return _juegoGatoState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _juegoGatoState extends State<juegoGato> {
+  List<List> _matriz;
+  _juegoGatoState(){
+    _iniciarMatriz();
   }
-
+  _iniciarMatriz(){
+    _matriz = List<List>(3);//Longitud de la lista
+    for(int i = 0; i < _matriz.length; i++){
+      _matriz[i] = List(3);
+      for(int j=0; j < _matriz[i].length; j++){
+        _matriz[i][j] = ' ';
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+            'Tres en línea'
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body:
+      Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildContenedorTablero(0,0),
+                _buildContenedorTablero(0,1),
+                _buildContenedorTablero(0,2),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildContenedorTablero(1,0),
+                _buildContenedorTablero(1,1),
+                _buildContenedorTablero(1,2),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildContenedorTablero(2,0),
+                _buildContenedorTablero(2,1),
+                _buildContenedorTablero(2,2),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  String _ultimoCaracter = 'o';
+  _buildContenedorTablero(int cordX, int cordY){
+    return GestureDetector(
+      onTap: (){
+        _rellenarTablero(cordX,cordY);
+        if(_revisarGanador(cordX,cordY)){
+          _mostrarVentana(_matriz[cordX][cordY]);
+
+        }
+        else{
+          if(_revisarDibujo()){
+            _mostrarVentana(null);
+          }
+        }
+
+      },
+      child: Container(
+        width: 90,
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            border: Border.all(
+                color: Colors.black12
+            )
+        ),
+        child:Center(
+          child :Text(
+            _matriz[cordX][cordY],
+            style: TextStyle(
+              fontSize: 92,
+
+            ),
+          ),
+        ),
+      ),
+    );
+
+  }
+  _mostrarVentana(String ganador) {
+    String texto;
+    if (ganador == null){
+      texto = 'Es un empate';
+    }
+    else{
+      texto = 'Jugador $ganador ganó';
+    }
+    showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text('Fin del juego'),
+            content: Text(texto),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _iniciarMatriz();
+                  });
+                },
+                child: Text('Nueva partida'),
+              )
+            ],
+          );
+
+        }
+    );
+  }
+  _rellenarTablero(int cordX, int cordY){
+    setState(() {
+      if(_matriz[cordX][cordY] == ' '){
+        if(_ultimoCaracter == 'o'){
+          _matriz[cordX][cordY] = 'x';
+        }
+        else{
+          _matriz[cordX][cordY] = 'o';
+        }
+        _ultimoCaracter = _matriz[cordX][cordY];
+      }
+    });
+  }
+  _revisarDibujo(){
+    bool dibujo = true;
+    _matriz.forEach((i){
+      i.forEach((j) {
+        if(j == ' ')
+          dibujo = false;
+      });
+    });
+    return dibujo;
+  }
+
+  _revisarGanador(int cordX, int cordY){
+    int col = 0, renglon = 0, diag = 0, rdiag = 0;
+    int n = _matriz.length-1;
+    String jugador = _matriz[cordX][cordY];
+
+    for(int i = 0; i < _matriz.length; i++){
+      if(_matriz[cordX][i] == jugador)
+        col++;
+      if(_matriz[i][cordY] == jugador)
+        renglon++;
+      if(_matriz[i][i] == jugador)
+        diag++;
+      if(_matriz[i][n-1] == jugador)
+        rdiag++;
+    }
+    if(renglon ==n+1 || col==n+1|| diag==n+1|| rdiag==n+1 ) {
+      return true;
+    }
+    return false;
+
+  }
 }
+
